a067f83422bd19f8a5ea542b6deaef8197855272
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-09-11 15:48:46 +0300

    Initial Commit

---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Beste Kocaoglu on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
